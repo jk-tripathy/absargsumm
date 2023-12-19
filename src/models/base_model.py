14220572402def410d6e29ddde1c0f91796fb150
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2023-12-20 05:20:17 +0530

    feat: add base model

---
+++ src/models/base_model.py
@@ -0,0 +1,20 @@
+import torch
+from torch import nn
+from transformer import AutoModel
+
+
+class BaseModel(nn.Module):
+    def __init__(self, model_name, frozen=True):
+        super(BaseModel, self).__init__()
+
+        self.model_name = model_name
+        self.frozen = frozen
+        self.model = AutoModel.from_pretrained(model_name)
+
+    def forward(self, x):
+        if not self.frozen:
+            logits = self.model(**x).logits
+        else:
+            with torch.no_grad():
+                logits = self.model(**x).logits
+        return logits
