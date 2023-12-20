89145dd4b37b699b2027006ee6bc89c0293d2628
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2023-12-21 00:39:48 +0530

    test: ensure that data actually works

--- tests/test_data.py
+++ tests/test_data.py
@@ -21,11 +21,50 @@ def datamodule(dataset_variant):
     return dm
 
 
-def test_train_dataloader(datamodule):
-    datamodule.setup("fit")
-    train_dataloader = datamodule.train_dataloader()
-    assert len(train_dataloader) == 2
-    assert train_dataloader.batch_size == 4
+@pytest.mark.parametrize("setup_stage", ["fit", "validate", "test", "predict"])
+def test_dataset(datamodule, setup_stage):
+    datamodule.setup(stage=setup_stage)
+    if setup_stage == "fit":
+        dataset = datamodule.train_dataset
+    elif setup_stage == "validate":
+        dataset = datamodule.val_dataset
+    elif setup_stage == "test":
+        dataset = datamodule.test_dataset
+    elif setup_stage == "predict":
+        dataset = datamodule.predict_dataset
 
-    batch_data = next(iter(train_dataloader))
+    sample = dataset[0]
+    assert type(sample["input_ids"]) is torch.Tensor
+    assert type(sample["attention_mask"]) is torch.Tensor
+    assert type(sample["token_type_ids"]) is torch.Tensor
+    assert type(sample["target"]) is torch.Tensor
+
+    assert sample["input_ids"].shape == torch.Size([512])
+    assert sample["attention_mask"].shape == torch.Size([512])
+    assert sample["token_type_ids"].shape == torch.Size([512])
+    assert sample["target"].shape == torch.Size([512])
+
+
+@pytest.mark.parametrize("setup_stage", ["fit", "validate", "test", "predict"])
+def test_dataloader(datamodule, setup_stage):
+    datamodule.setup(stage=setup_stage)
+    if setup_stage == "fit":
+        dataloader = datamodule.train_dataloader()
+    elif setup_stage == "validate":
+        dataloader = datamodule.val_dataloader()
+    elif setup_stage == "test":
+        dataloader = datamodule.test_dataloader()
+    elif setup_stage == "predict":
+        dataloader = datamodule.predict_dataloader()
+
+    batch_data = next(iter(dataloader))
+
     assert type(batch_data["input_ids"]) is torch.Tensor
+    assert type(batch_data["attention_mask"]) is torch.Tensor
+    assert type(batch_data["token_type_ids"]) is torch.Tensor
+    assert type(batch_data["target"]) is torch.Tensor
+
+    assert batch_data["input_ids"].shape == torch.Size([4, 512])
+    assert batch_data["attention_mask"].shape == torch.Size([4, 512])
+    assert batch_data["token_type_ids"].shape == torch.Size([4, 512])
+    assert batch_data["target"].shape == torch.Size([4, 512])
