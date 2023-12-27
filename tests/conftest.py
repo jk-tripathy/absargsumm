708c4a67e0321f214f97de46e6f2c3f855117dfe
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2023-12-27 23:46:44 +0530

    fix: set up pyroot properly for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,4 @@
-import io
-
 import pyrootutils
-import pytest
-
-from utils import parser
 
 root = pyrootutils.setup_root(
     search_from=__file__,
@@ -13,6 +8,10 @@ root = pyrootutils.setup_root(
     pythonpath=True,
     cwd=True,
 )
+
+import pytest
+
+from utils import parser
 
 
 @pytest.fixture()
@@ -37,3 +36,7 @@ def test_parser_args(parser_args):
     assert parser_args.batch_size == 4
     assert parser_args.stage == "fit"
     assert parser_args.dataset_limit == 32
+    assert parser_args.model_name == "bert-base-uncased"
+    assert parser_args.frozen is True
+    assert parser_args.learning_rate == 3e-4
+    assert parser_args.num_workers == 0
