fix: masking now (actually) works

models/SimpleTransformer/simple_trasformer_model.py
@@ -35,19 +35,29 @@ class SimpleTransformer(pl.LightningModule):
         self.loss = torch.nn.CrossEntropyLoss()
 
     def generate_square_subsequent_mask(self, sz):
-        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
+        mask = torch.triu(torch.ones(1, sz, sz)) == 1
         mask = (
             mask.float().masked_fill(mask == 0, float("-inf")).masked_fill(mask == 1, float(0.0))
         )
         return mask.bool()
 
-    def create_masks(self, src_attn_mask, tgt_attn_mask):
-        src_attn_mask = src_attn_mask.bool()
-        tgt_attn_mask = tgt_attn_mask.bool()
-        src_mask = src_attn_mask & self.generate_square_subsequent_mask(src_attn_mask.size(-1))
-        tgt_mask = tgt_attn_mask & self.generate_square_subsequent_mask(tgt_attn_mask.size(-1))
-        src_mask = src_mask.to(src_attn_mask.device)
-        tgt_mask = tgt_mask.to(tgt_attn_mask.device)
+    def create_masks(self, src_attn_mask, tgt_attn_mask=None):
+        src_attn_mask = src_attn_mask.unsqueeze(1)
+        src_sub_mask = self.generate_square_subsequent_mask(src_attn_mask.size(-1))
+        src_mask = src_attn_mask & src_sub_mask
+        # why
+        # why must i do this?
+        # what does this even mean
+        # TODO: pray for salvation
+        src_mask = src_mask.repeat(self.base_config.num_attention_heads, 1, 1)
+        src_mask = src_mask.bool().to(src_attn_mask.device)
+
+        if tgt_attn_mask is not None:
+            tgt_attn_mask = tgt_attn_mask.unsqueeze(1)
+            tgt_sub_mask = self.generate_square_subsequent_mask(tgt_attn_mask.size(-1))
+            tgt_mask = tgt_attn_mask & tgt_sub_mask
+            tgt_mask = tgt_mask.repeat(self.base_config.num_attention_heads, 1, 1)
+            tgt_mask = tgt_mask.bool().to(tgt_attn_mask.device)
         return src_mask, tgt_mask
 
     def forward(self, src, src_attn_mask, tgt, tgt_attn_mask):
@@ -55,13 +65,13 @@ class SimpleTransformer(pl.LightningModule):
 
         src_embed_out = self.encoder_embed(src)
         tgt_embed_out = self.decoder_embed(tgt)
-        enc_out = self.encoder(src_embed_out)
+        enc_out = self.encoder(src_embed_out, mask=src_mask)
         dec_out_logits = self.decoder(
             tgt_embed_out, enc_out, tgt_mask=tgt_mask, memory_mask=src_mask
         )
         dec_out_logits = self.linear(dec_out_logits)
 
-        loss = self.loss(dec_out_logits, tgt)
+        loss = self.loss(dec_out_logits.permute(0, 2, 1), tgt)
 
         return dec_out_logits, loss
 

tests/conftest.py
@@ -10,6 +10,7 @@ root = pyrootutils.setup_root(
 )
 
 import pytest
+import torch
 from transformers import BertTokenizer
 
 from utils.parser import parser
@@ -47,22 +48,37 @@ def test_parser_args(parser_args):
 def batch_str():
     return [
         "Hello, my dog is cute",
+        "Hello, my cat is also cute",
     ]
 
 
 @pytest.fixture()
 def batch(batch_str):
     tokenizer = BertTokenizer.from_pretrained("bert-base-uncased", use_fast=True)
-    tokenized = tokenizer(
-        batch_str,
-        return_tensors="pt",
-        padding="max_length",
-        truncation=True,
-        max_length=512,
-    )
-    return {
-        "src": tokenized["input_ids"].flatten(),
-        "src_attn_mask": tokenized["attention_mask"].flatten(),
-        "tgt": tokenized["input_ids"].flatten(),
-        "tgt_attn_mask": tokenized["attention_mask"].flatten(),
+    data = {
+        "src": [],
+        "src_attn_mask": [],
+        "tgt": [],
+        "tgt_attn_mask": [],
     }
+    for str in batch_str:
+        tokenized = tokenizer(
+            str,
+            return_tensors="pt",
+            padding="max_length",
+            truncation=True,
+            max_length=512,
+        )
+        data["src"].append(tokenized["input_ids"].flatten())
+        data["src_attn_mask"].append(tokenized["attention_mask"].flatten())
+        data["tgt"].append(tokenized["input_ids"].flatten())
+        data["tgt_attn_mask"].append(tokenized["attention_mask"].flatten())
+
+    tensor_data = {
+        "src": torch.stack(data["src"]),
+        "src_attn_mask": torch.stack(data["src_attn_mask"]),
+        "tgt": torch.stack(data["tgt"]),
+        "tgt_attn_mask": torch.stack(data["tgt_attn_mask"]),
+    }
+
+    return tensor_data