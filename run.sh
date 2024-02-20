09710240efb2adb1d1ab2e129b63f877e223551c
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2024-02-20 20:54:01 +0100

    fix: wrong column names

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ python main.py \
     --dataset cnn_dailymail \
     --dataset_variant 3.0.0 \
     --dataset_limit 32 \
-    --shorttext_column article \
-    --summary_column highlights \
+    --longtext_column article \
+    --shorttext_column highlights \
     --guidance gsum \
     --batch_size 4 \
     --stage fit \
