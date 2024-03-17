1893b7b09c54a2aba0b86102bf56f358e20b0aac
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2024-03-17 15:40:11 +0100

    feat: no simple shit

--- models/SimpleTransformer/__init__.py
+++
@@ -1,4 +0,0 @@
-from models.SimpleTransformer.configuration_simple_transformer import (
-    SimpleTransformerConfig,
-)
-from models.SimpleTransformer.modeling_simple_transformer import SimpleTransformer
