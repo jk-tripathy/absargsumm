5e63cc818793aaca5c962cee1dbe28b0e2d29bfd
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2024-03-22 14:04:58 +0100

    fix: token ids were not registered

--- models/GSum/configuration_gsum.py
+++ models/GSum/configuration_gsum.py
@@ -14,15 +14,15 @@ class GSumConfig(PretrainedConfig):
         num_decoder_layers=12,
         batch_first=True,
         pretrained_encoder_name_or_path="bert-base-uncased",
-        pad_token_id=0,
-        bos_token_id=1,
-        eos_token_id=2,
         freeze_encoder=False,
         dropout=0.2,
         layer_norm_eps=1e-6,
         encoder_learning_rate=0.002,
         decoder_learning_rate=0.2,
         warmup_steps=15000,
+        pad_token_id=0,
+        bos_token_id=1,
+        eos_token_id=2,
         sep_token="[SEP]",
         cls_token="[CLS]",
         pad_token="[PAD]",
@@ -40,13 +40,15 @@ class GSumConfig(PretrainedConfig):
         self.num_decoder_layers = num_decoder_layers
         self.batch_first = batch_first
         self.pretrained_encoder_name_or_path = pretrained_encoder_name_or_path
-        self.pad_token_id = pad_token_id
         self.freeze_encoder = freeze_encoder
         self.dropout = dropout
         self.layer_norm_eps = layer_norm_eps
         self.encoder_learning_rate = encoder_learning_rate
         self.decoder_learning_rate = decoder_learning_rate
         self.warmup_steps = warmup_steps
+        self.pad_token_id = pad_token_id
+        self.bos_token_id = bos_token_id
+        self.eos_token_id = eos_token_id
         self.sep_token = sep_token
         self.cls_token = cls_token
         self.pad_token = pad_token
@@ -62,13 +64,15 @@ class GSumConfig(PretrainedConfig):
             "nhead": self.nhead,
             "batch_first": self.batch_first,
             "pretrained_encoder_name_or_path": self.pretrained_encoder_name_or_path,
-            "pad_token_id": self.pad_token_id,
             "freeze_encoder": self.freeze_encoder,
             "dropout": self.dropout,
             "layer_norm_eps": self.layer_norm_eps,
             "encoder_learning_rate": self.encoder_learning_rate,
             "decoder_learning_rate": self.decoder_learning_rate,
             "warmup_steps": self.warmup_steps,
+            "pad_token_id": self.pad_token_id,
+            "bos_token_id": self.bos_token_id,
+            "eos_token_id": self.eos_token_id,
             "sep_token": self.sep_token,
             "cls_token": self.cls_token,
             "pad_token": self.pad_token,
